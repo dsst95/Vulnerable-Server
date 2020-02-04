19d022d0c4c1661d08cb40df69a9d45f32c0ff51
Dennis Stumm <6422711+dennis95stumm@users.noreply.github.com>
2020-02-04 19:05:13 +0100

    Update smart_server.c

--- smart_server.c
+++ smart_server.c
@@ -1,3 +1,28 @@
+/**
+ * @file smart_server.c
+ * @author Dennis Stumm
+ * @brief This file contains a vulnerable socket server. To run the server
+ *   compile it and start with as following: ./NAME PORTNUMBER
+ * @version 1.0
+ * @date 2020-02-04
+ * 
+ * @copyright Copyright (c) 2020 Dennis Stumm
+ *******************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *******************************************************************************
+ */
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
